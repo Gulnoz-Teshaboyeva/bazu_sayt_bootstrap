a6c71e09b76dd819330099564e59ab34c1ab15c1
Gulnoz-Teshaboyeva <teshaboyeva.gulnoz@gmail.com>
2021-11-19 22:23:20 +0500

    Bootstrap site

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+// var poisk = $('.seller-left i');
+// var btn = $('.btnAdd');
+// var card = $('.pic').find('.card');
+// var images = $('.images');
+
+// console.log(btn);
+
+// var img = $('.chestrSofa')
+// console.log(img);
+
+
+// img.on('mouseover' , function(){
+//     poisk.css("display" , "block").text();
+//     poisk.css("margin" , "auto").text();
+    
+// });
+// poisk.on('click', function(){
+//     img.css("transform", "scale(1.2)").text();
+//     img.css("transition-duration", "2s" ).text();
+//     btn.css("display" , "block").text();
+//     card.css("height" , "400px");
+//     images.css("display" , "block").text();
+//     poisk.css("top" , "20%").text();
+//     card.css("position" , "absolute").text();
+   
+// })
+
+
+
+
+})
+
+    
